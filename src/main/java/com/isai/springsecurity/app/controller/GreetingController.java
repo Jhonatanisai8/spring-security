273f92684b68e9f65715fe83f3898e62cc516e65
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-02-22 02:18:01 -0500

    saludo de prueba

---
+++ src/main/java/com/isai/springsecurity/app/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package com.isai.springsecurity.app.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/greeting")
+public class GreetingController {
+
+    @GetMapping("/sayHello")
+    public String sayHello() {
+        return "Hello World";
+    }
+}
